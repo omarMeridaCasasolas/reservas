1bc592e9a864ab9c5e68a36174da02490f51965c
OmarCasasolasM <omar.casasolas@sincpro.com.bo>
2022-09-01 02:45:29 -0400

    DB remoto

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -4,9 +4,18 @@
         protected $connexion_bd;
         public function Conexion(){
             try {
-                $usuario = "root";
-                $contrasenia = "kirium";
-                $this->connexion_bd = new PDO('mysql:host=localhost;dbname=reserva;port=3307',$usuario,$contrasenia);
+                // $usuario = "root";
+                // $contrasenia = "kirium";
+                // $this->connexion_bd = new PDO('mysql:host=localhost;dbname=reserva;port=3307',$usuario,$contrasenia);
+
+                // $usuario = "uasrxz4qrh1xinnr";
+                // $contrasenia = "WSM9ks7dz2mQJcZ9DUCy";
+                // $this->connexion_bd = new PDO('mysql:host=bxqsxwdoasmaf0zorzvi;dbname=reserva;port=3306',$usuario,$contrasenia);
+
+                $usuario = "uasrxz4qrh1xinnr";
+                $contrasenia = "WSM9ks7dz2mQJcZ9DUCy";
+                $this->connexion_bd = new PDO('mysql:host=bxqsxwdoasmaf0zorzvi-mysql.services.clever-cloud.com;dbname=bxqsxwdoasmaf0zorzvi;port=3306',$usuario,$contrasenia);
+
                 $this->connexion_bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $this->connexion_bd;
             }catch (PDOException $e) {
